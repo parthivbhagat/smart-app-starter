176a80d5b0869848a9af9a6866798aa1bebc9a59
Parthiv Bhagat <parthiv.bhagat@cerner.com>
2016-08-06 16:12:44 -0500

    Made code review changes

--- load_data.js
+++ load_data.js
@@ -7,44 +7,55 @@
       var pt = patient.read();
      
       $.when(pt).done(function(patient){
-		var gender = patient.gender;
-		var dob = new Date(patient.birthDate);		 
-		var day = dob.getDate();
-		var monthIndex = dob.getMonth();
-		var year = dob.getFullYear();
+        var gender = patient.gender;
+        var dob = new Date(patient.birthDate);     
+        var day = dob.getDate();
+        var monthIndex = dob.getMonth() + 1;
+        var year = dob.getFullYear();
 
-		var dobStr = monthIndex + '/' + day + '/' + year;
-		
-		var fname = patient.name[0].given.join(" ");
-		var lname = patient.name[0].family.join(" ");
-		var age = calculateAge(dob);
-		
-		p = defaultPatient();
-		p.birthday = {value:dobStr};
-		p.gender={value:gender};
-		p.givenName={value:fname};
-		p.familyName={value:lname};
-		p.age = {value:age};
-		ret.resolve(p);
-		
+        var dobStr = monthIndex + '/' + day + '/' + year;
+        
+        var fname = patient.name[0].given.join(" ");
+        var lname = patient.name[0].family.join(" ");
+        var age = parseInt(calculateAge(dob));
+        
+        p = defaultPatient();
+        p.birthday = {value:dobStr};
+        p.gender = {value:gender};
+        p.fname = {value:fname};
+        p.lname = {value:lname};
+        p.age = {value:age};
+        ret.resolve(p);
+    
       });
     });
     return ret.promise();
   };
   
-  function calculateAge(birthday) { // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
+  function isLeapYear(year) {
+    return new Date(year, 1, 29).getMonth() == 1;
+  }
+
+  function calculateAge(date) {
+    var d = new Date(date), now = new Date();
+    var years = now.getFullYear() - d.getFullYear();
+    d.setFullYear(d.getFullYear() + years);
+    if (d > now) {
+        years--;
+        d.setFullYear(d.getFullYear() - 1);
+    }
+    var days = (now.getTime() - d.getTime()) / (3600 * 24 * 1000);
+    return years + days / (isLeapYear(now.getFullYear()) ? 366 : 365);
+  }
+
   
   function defaultPatient(){
-	return {
-      'givenName':    {'value': null}
-	  ,'familyName':  {'value': null}
-	  ,'gender':      {'value': null}
-	  ,'birthday':    {'value': null}
-	  ,'age':         {'value': null}
+  return {
+     'fname' : {'value': null}
+    ,'lname' : {'value': null}
+    ,'gender' : {'value': null}
+    ,'birthday' : {'value': null}
+    ,'age' : {'value': null}
     }
   };
 
