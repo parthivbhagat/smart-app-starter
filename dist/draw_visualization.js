55c17e494886b27a33b7c71a397748b9510a36af
Parthiv Bhagat <parthiv.bhagat@cerner.com>
2016-08-12 12:37:15 -0500

    Added transpiled code through bable

---
+++ dist/draw_visualization.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*jshint esversion: 6 */
+function drawVisualization(p) {
+    var table = '<table><thead><th>First Name</th><th>Last Name</th><th>Gender</th><th>Birth Date</th><th>Age</th><th>Height</th></thead><tbody>';
+    table += '<tr>';
+    table += '<td>' + p.fname.value + '</td>';
+    table += '<td>' + p.lname.value + '</td>';
+    table += '<td>' + p.gender.value + '</td>';
+    table += '<td>' + p.birthday.value + '</td>';
+    table += '<td>' + p.age.value + '</td>';
+    table += '<td>' + p.height.value + '</td>';
+    table += '</tr>';
+    table += '</tbody></table>';
+    $('#holder').html(table);
+}
